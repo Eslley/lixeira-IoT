d9893225b5d5540e1426228bc271a6103b038760
eslleyrubem <rubemeslley@gmail.com>
2022-10-01 17:37:00 -0300

    Preparando para deploy(definindo STATICFILES_STORAGE)

--- LixeiraIoT/settings.py
+++ LixeiraIoT/settings.py
@@ -131,6 +131,8 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
