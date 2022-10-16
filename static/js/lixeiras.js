86271f6835f20af7c8f25b4b91b2e2949bce5480
eslleyrubem <rubemeslley@gmail.com>
2022-10-16 19:04:56 -0300

    Removendo método não utilizado em lixeiras.js

--- static/js/lixeiras.js
+++ static/js/lixeiras.js
@@ -31,55 +31,7 @@ var v = new Vue({
             http.get('lixeiras/').then(response => {
                 this.lixeiras = response.data;
             })
-        },
-
-        showAlert(type, title, message) {
-
-            switch(type) {
-              case "success":
-                Swal.fire({
-                  title: title,
-                  text: message,
-                  icon: 'success',
-                  iconColor: '#f5f5f5',
-                  timer: 4000,
-                  showConfirmButton: false,
-                  toast: true,
-                  position: 'top',
-                  color: '#f5f5f5',
-                  background: '#77b93f'
-                })
-                break
-              case "error":
-                Swal.fire({
-                  title: title,
-                  text: message,
-                  icon: 'error',
-                  iconColor: '#f5f5f5',
-                  timer: 4000,
-                  showConfirmButton: false,
-                  toast: true,
-                  position: 'top',
-                  color: '#f5f5f5',
-                  background: '#ff4f52'
-                })
-                break
-              case "warning":
-                Swal.fire({
-                  title: title,
-                  text: message,
-                  icon: 'warning',
-                  iconColor: '#f5f5f5',
-                  timer: 4000,
-                  showConfirmButton: false,
-                  toast: true,
-                  position: 'top',
-                  color: '#f5f5f5',
-                  background: '#ff4f52'
-                })
-                break
-            }
-          }
+        }
         
     }
 })
