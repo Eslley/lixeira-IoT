21e5a5ce9af2daba053bffd15101671c236e5ac3
LordWaif <39893326+LordWaif@users.noreply.github.com>
2022-10-20 18:01:15 -0300

    Update ProjetoLixeira.ino

--- LixeiraIoT/código fonte arduino/ProjetoLixeira.ino
+++ LixeiraIoT/código fonte arduino/ProjetoLixeira.ino
@@ -1,27 +1,31 @@
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <Servo.h>
+
+#include <ESP32Servo.h>
 #include "Ultrasonic.h"
-#include <ESP8266HTTPClient.h>
+
+#include <WiFi.h>
+
+#include <HTTPClient.h>
 
 #ifndef STASSID
-#define STASSID "jassonCell"
-#define STAPSK  "ratinhas"
+#define STASSID "SSID"
+#define STAPSK "PASSWORD"
 #endif
 
-#define SERVO D3 // Porta Digital 6 PWM
-#define TRIGGER D4
-#define ECHO D5
-#define FECHADO 45
-#define ABERTO 140
+#define ITV 1
+#define SERVO 13 // Porta Digital 6 PWM
+#define TRIGGER 12
+#define ECHO 15
+#define FECHADO 70
+#define ABERTO 170
+#define TAM 14
 
 const int ID_LIXEIRA_1 = 1;
 
 const char *ssid = STASSID;
 const char *password = STAPSK;
 
-const int pinTrigger = D6;
-const int pinEcho = D7;
+const int pinTrigger = 27;
+const int pinEcho = 26;
 
 bool isOpen = false;
 int nivel = 0;
@@ -40,12 +44,13 @@ void requestLixeiraUpdate(int id, bool isOpen, int nivel){
     String url = "https://lixeira-iot.herokuapp.com/api/lixeiras/update/";
 
     String open = isOpen ? "true" : "false";
-
+    Serial.println(nivel);    
+    nivel = 100 - nivel;
     String json = "{\"id\": " + String(ID_LIXEIRA_1)  + ", \"estaAberta\": " + open + ", \"nivel\": " + String(nivel) + "}";
 
     Serial.println(json);
     
-    http.begin(url, "B5 86 32 41 D3 FB 3D 2B 56 26 0E A3 51 14 75 E8 DC FD 4A 9B"); 
+    http.begin(url); 
     http.addHeader("Content-Type", "application/json");
     http.setTimeout(10000);
 
@@ -69,55 +74,54 @@ void requestLixeiraUpdate(int id, bool isOpen, int nivel){
     http.end(); 
   }
 }
-
-void setup(void) {
-  s.attach(SERVO);
-  s.write(FECHADO); // Inicia motor posição zero
-
-  Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
+//HTTPClient http;
+//WiFiClient client;
+void setup() {
+	// Allow allocation of all timers
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);  
   WiFi.begin(ssid, password);
-  Serial.println("");
-
+	//s.setPeriodHertz(50);    // standard 50 hz servo
+	s.attach(SERVO); // attaches the servo on pin 18 to the servo object
+  s.write(FECHADO);  
+	
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Connecting...");
-  }
-
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  } 
+  Serial.println("Conneted");   
 }
 
 void loop(void) {
+  //http.begin("https://lixeira-iot.herokuapp.com/api/lixeiras/");   
+  //int httpCode = http.GET();
+  //http.setTimeout(10000);  
+  //Serial.println(httpCode);
   int distancia = sensor1.distance();
-  Serial.print("Distancia em cm: ");
+  //Serial.print("Distancia em cm: ");
   Serial.println(distancia);
   delay(50); // Velocidade de leitura (evita trepidações na porta)
-
+  //Serial.println(nivel);
   if(distancia > 20){
     isOpen = false;
-    if(s.read()==ABERTO){
+    if(s.read()==ABERTO-ITV){
       for(pos = ABERTO; pos >= FECHADO; pos--){
         s.write(pos);
         delay(15); //Velocidade de fechamento
       }
-
-      nivel = (sonar.distance() * 100) / 30; // considerando que a lixeira tem 30cm de altura
-      
-      requestLixeiraUpdate(ID_LIXEIRA_1, isOpen, nivel);
+       // considerando que a lixeira tem 15cm de altura
+      //requestLixeiraUpdate(ID_LIXEIRA_1, isOpen, nivel);      
     } 
+    nivel = (sonar.distance() * 100) / TAM;
+    requestLixeiraUpdate(ID_LIXEIRA_1, isOpen, nivel);    
     }else{
       isOpen = true;
-      if(s.read()==FECHADO){
+      if(s.read()==FECHADO-ITV){        
         for(pos = FECHADO; pos <= ABERTO; pos++){
           s.write(pos);
           delay(5); //Velocidade de abertura
         }
-
         requestLixeiraUpdate(ID_LIXEIRA_1, isOpen, nivel);
       }
       delay(2000); //Tempo de permanencia aberto minimo depois de totalmente aberto
